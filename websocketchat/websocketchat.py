a342b2bcf61b2f68784cd64f9810a0aa5fe09db5
Hayden Eastwell <haydeneastwell@outlook.com>
2024-01-10 09:47:38 +0000

    Fixed test runs by adding __main__ check in websocketchat.py

--- websocketchat/websocketchat.py
+++ websocketchat/websocketchat.py
@@ -54,4 +54,5 @@ async def shutdown_event_handler():
     redis_connection.close()
 
 
-uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
